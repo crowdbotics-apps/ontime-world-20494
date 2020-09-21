0891f204f79f8a052565b8bdfafbab4ccf0c6912
Crowdbotics <team@crowdbotics.com>
2020-09-21 05:07:14 +0000

    Update models

--- backend/ontime_world_20494/wsgi.py
+++ backend/ontime_world_20494/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ontime_world_20494.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ontime_world_20494.settings")
 
 application = get_wsgi_application()
